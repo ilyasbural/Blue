818a320ee84b028136dcc8b23776a826c0e3ffaa
Ilyas Bural <ilyasbural@gmail.com>
2023-12-16 02:22:38 +0300

    BuildingTypeController codes edited

--- Blue.Presentation/Areas/Management/Controllers/BuildingTypeController.cs
+++ Blue.Presentation/Areas/Management/Controllers/BuildingTypeController.cs
@@ -1,37 +1,34 @@
 ﻿namespace Blue.Presentation.Areas.Management.Controllers
 {
-    using Core;
-    using Microsoft.AspNetCore.Mvc;
+	using Core;
+	using Microsoft.AspNetCore.Mvc;
 
-    [Area("Management")]
-    public class BuildingTypeController : Controller
-    {
-        readonly IBuildingTypeService Service;
-        public BuildingTypeController(IBuildingTypeService service)
-        {
-            Service = service;
-        }
+	[Area("Management")]
+	public class BuildingTypeController : Controller
+	{
+		readonly IBuildingTypeService Service;
+		public BuildingTypeController(IBuildingTypeService service)
+		{
+			Service = service;
+		}
 
-        public async Task<IActionResult> Index()
-        {
-            var Model = Tuple.Create<List<BuildingTypeViewModel>> (new List<BuildingTypeViewModel>());
-            Response<BuildingType> BuildingTypeResponse = await Service.SelectAsync(new BuildingTypeSelectDto {  });
-            foreach (BuildingType BuildingType in BuildingTypeResponse.Collection)
-            {
-                BuildingTypeViewModel ViewModel = new BuildingTypeViewModel { Id = BuildingType.Id };
-                Model.Item1.Add(ViewModel);
-            }
-            return View(Model);
-        }
+		public async Task<IActionResult> Index()
+		{
+			var Model = Tuple.Create<List<BuildingTypeViewModel>>(new List<BuildingTypeViewModel>());
+			Response<BuildingType> BuildingTypeResponse = await Service.SelectAsync(new BuildingTypeSelectDto { });
+			foreach (BuildingType BuildingType in BuildingTypeResponse.Collection)
+			{
+				BuildingTypeViewModel ViewModel = new BuildingTypeViewModel { Id = BuildingType.Id };
+				Model.Item1.Add(ViewModel);
+			}
+			return View(Model);
+		}
 
-        public IActionResult Create()
-        {
-            var Model = Tuple.Create<BuildingTypeViewModel>(new BuildingTypeViewModel());
-
-            //await Service.InsertAsync(new BuildingTypeRegisterDto { });
-
-            return View(Model);
-        }
+		public IActionResult Create()
+		{
+			var Model = Tuple.Create<BuildingTypeViewModel>(new BuildingTypeViewModel());
+			return View(Model);
+		}
 
 		[HttpPost]
 		public async Task<IActionResult> Create([Bind(Prefix = "Item1")] BuildingTypeViewModel Model)
@@ -43,18 +40,18 @@
 			else return View(Model);
 		}
 
-		public IActionResult Update() 
-        {
-            var Model = Tuple.Create<BuildingTypeViewModel>(new BuildingTypeViewModel());
+		public IActionResult Update()
+		{
+			var Model = Tuple.Create<BuildingTypeViewModel>(new BuildingTypeViewModel());
 
-            return View(Model); 
-        }
+			return View(Model);
+		}
 
-        public IActionResult Delete()
-        {
-            var Model = Tuple.Create<BuildingTypeViewModel>(new BuildingTypeViewModel());
+		public IActionResult Delete()
+		{
+			var Model = Tuple.Create<BuildingTypeViewModel>(new BuildingTypeViewModel());
 
-            return View(Model);
-        }
-    }
+			return View(Model);
+		}
+	}
 }
