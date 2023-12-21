d913ee1b9fb1bc030d7e5986288951db0b81b668
Ilyas Bural <ilyasbural@gmail.com>
2023-12-22 00:18:19 +0300

    Controllers edited

--- Blue.Presentation/Areas/Management/Controllers/WarmingController.cs
+++ Blue.Presentation/Areas/Management/Controllers/WarmingController.cs
@@ -1,44 +1,44 @@
 ﻿namespace Blue.Presentation.Areas.Management.Controllers
 {
-	using Core;
-	using Microsoft.AspNetCore.Mvc;
+    using Core;
+    using Microsoft.AspNetCore.Mvc;
 
-	[Area("Management")]
-	public class WarmingController : Controller
-	{
-		readonly IWarmingService Service;
-		public WarmingController(IWarmingService service)
-		{
-			Service = service;
-		}
+    [Area("Management")]
+    public class WarmingController : Controller
+    {
+        readonly IWarmingService Service;
+        public WarmingController(IWarmingService service)
+        {
+            Service = service;
+        }
 
-		public async Task<IActionResult> Index()
-		{
-			var Model = Tuple.Create<List<WarmingViewModel>>(new List<WarmingViewModel>());
-			Response<Warming> WarmingResponse = await Service.SelectAsync(new WarmingSelectDto { });
-			foreach (Warming Warming in WarmingResponse.Collection)
-			{
-				WarmingViewModel ViewModel = new WarmingViewModel { Id = Warming.Id, Name = Warming.Name };
-				Model.Item1.Add(ViewModel);
-			}
-			return View(Model);
-		}
+        public async Task<IActionResult> Index()
+        {
+            var Model = Tuple.Create<List<WarmingViewModel>>(new List<WarmingViewModel>());
+            Response<Warming> WarmingResponse = await Service.SelectAsync(new WarmingSelectDto { });
+            foreach (Warming Warming in WarmingResponse.Collection)
+            {
+                WarmingViewModel ViewModel = new WarmingViewModel { Id = Warming.Id, Name = Warming.Name };
+                Model.Item1.Add(ViewModel);
+            }
+            return View(Model);
+        }
 
-		public IActionResult Create()
-		{
-			var Model = Tuple.Create<WarmingViewModel>(new WarmingViewModel());
-			return View(Model);
-		}
+        public IActionResult Create()
+        {
+            var Model = Tuple.Create<WarmingViewModel>(new WarmingViewModel());
+            return View(Model);
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> Create([Bind(Prefix = "Item1")] WarmingViewModel Model)
-		{
-			WarmingRegisterDto Warming = new WarmingRegisterDto();
-			Warming.Name = Model.Name;
-			Response<Warming> Response = await Service.InsertAsync(Warming);
-			if (Response.Success > 0) return RedirectToAction("Index");
-			else return View();
-		}
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind(Prefix = "Item1")] WarmingViewModel Model)
+        {
+            WarmingRegisterDto Warming = new WarmingRegisterDto();
+            Warming.Name = Model.Name;
+            Response<Warming> Response = await Service.InsertAsync(Warming);
+            if (Response.Success > 0) return RedirectToAction("Index");
+            else return View();
+        }
 
         public async Task<IActionResult> Update(Guid Id)
         {
@@ -54,9 +54,9 @@
         }
 
         public async Task<IActionResult> Delete(Guid Id)
-		{
-			var Model = Tuple.Create<WarmingViewModel>(new WarmingViewModel());
-			return View(Model);
-		}
-	}
+        {
+            var Model = Tuple.Create<WarmingViewModel>(new WarmingViewModel());
+            return View(Model);
+        }
+    }
 }
